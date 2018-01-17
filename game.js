e9f8cdb1f3241c8548eb4eab3d2238c772d986ec
bzykuu <35331669+bzykuu@users.noreply.github.com>
2018-01-17 11:21:04 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
 var map = [];
+map = [[3, 5, 7, 5, 4, 6],
+       [2, 3, 6, 2, 4, 5],
+       [8, 5, 1, 6, 1, 4],
+       [1, 7, 8, 3, 7, 3],
+       [9, 1, 4, 1, 3, 2],
+       [3, 3, 2, 2, 5, 1]];
+var Ant = function(name) {
+  this.name = name;
+  this.position = [0, 0];
+};
+var ant0 = new Ant("0");
