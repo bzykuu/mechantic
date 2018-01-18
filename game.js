084db7c7b8f7e057157fa9c85a7eebb26b7dff68
bzykuu <35331669+bzykuu@users.noreply.github.com>
2018-01-18 12:35:04 +0100

    a

--- game.js
+++ game.js
@@ -66,3 +66,8 @@ tekst = document.getElementById("temp");
 tekst.innerHTML = "Anthill is here: " + world1.map[0][0].ant.anthill;
 
 drawMap(world1.map);
+
+var para = document.createElement("P");
+para.id = "temp2";
+para.appendChild(document.createTextNode("Anthill is here: " + world1.map[0][0].ant.anthill));
+document.body.appendChild(para);
